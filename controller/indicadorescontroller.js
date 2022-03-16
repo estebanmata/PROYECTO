cdef39043884a20f1a0bf9e03f103cab8a63eb16
estebanmata <ematamoros1989@hotmail.com>
2022-03-15 21:34:51 -0600

    Se agrega repositorio en el main

---
+++ controller/indicadorescontroller.js
@@ -0,0 +1,11 @@
+const indicadoresEconomicosBCCR = require('../models/indicadores');
+
+indicadoresEconomicosBCCR().then((data) => {
+    console.log("Compra y Venta del día de hoy:");
+    console.log(data);
+});
+
+indicadoresEconomicosBCCR('15/03/2022', '31/12/2022').then((data) => {
+    console.log("Compra y Venta por fecha:");
+    console.log(data);
+});
